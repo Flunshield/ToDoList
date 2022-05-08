c12687bf4ef94d8b5edb12f781d4c12d42518c17
Julien BERTRAND <j.bertrand.sin@gmail.com>
2022-05-08 19:02:24 +0200

    ajout function suppr

--- test.js
+++ test.js
@@ -20,3 +20,23 @@ function Ajout()
         b++;
         }
 }
+
+function Suprimer()
+{
+    // Sélectionner l'élément input et récupérer sa valeur
+    let valeur = document.getElementById("ajouter").value;
+
+    todolist.push(valeur);
+
+    const newTache = document.createElement("li");
+    let tache = document.getElementById("input1");
+    let k = tache.appendChild(newTache);
+    let lenght = todolist.length;
+    console.log(lenght);
+    let b=0;
+        for (let i = 0; i < lenght; i++)
+        {
+        k.innerHTML = todolist[b];
+        b++;
+        }
+}
