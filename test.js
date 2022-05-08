50cc57687bad77fc270292367d6c308d7f7e7c64
Julien BERTRAND <j.bertrand.sin@gmail.com>
2022-05-08 19:00:45 +0200

    V2

---
+++ test.js
@@ -0,0 +1,22 @@
+
+var todolist = [];
+
+function Ajout()
+{
+    // Sélectionner l'élément input et récupérer sa valeur
+    let valeur = document.getElementById("ajouter").value;
+
+    todolist.push(valeur);
+
+    const newTache = document.createElement("li");
+    let tache = document.getElementById("input1");
+    let k = tache.appendChild(newTache);
+    let lenght = todolist.length;
+    console.log(lenght);
+    let b=0;
+        for (let i = 0; i < lenght; i++)
+        {
+        k.innerHTML = todolist[b];
+        b++;
+        }
+}
