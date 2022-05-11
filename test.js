243f4386c2e0268ba54a0da79f68109330b48c7b
Julien BERTRAND <j.bertrand.sin@gmail.com>
2022-05-11 11:07:55 +0200

    ajout de la fonction supprimer

--- test.js
+++ test.js
@@ -1,5 +1,24 @@
 
-var todolist = [];
+const todolist = [];
+
+function clean ()
+{
+    const newTache = document.createElement("li");
+    const tache = document.getElementById("input1");
+    let k = tache.appendChild(newTache);
+
+    let lenght = todolist.length;
+    console.log(lenght);
+
+    let b=0;
+        for (let i = 0; i < lenght; i++)
+        {
+        k.innerHTML = todolist[b];
+        b++;
+        }
+        
+    console.log(todolist);
+}
 
 function Ajout()
 {
@@ -7,36 +26,20 @@ function Ajout()
     let valeur = document.getElementById("ajouter").value;
 
     todolist.push(valeur);
+    clean();
 
-    const newTache = document.createElement("li");
-    let tache = document.getElementById("input1");
-    let k = tache.appendChild(newTache);
-    let lenght = todolist.length;
-    console.log(lenght);
-    let b=0;
-        for (let i = 0; i < lenght; i++)
-        {
-        k.innerHTML = todolist[b];
-        b++;
-        }
 }
 
 function Suprimer()
 {
-    // Sélectionner l'élément input et récupérer sa valeur
-    let valeur = document.getElementById("ajouter").value;
+    let valeur = document.getElementById("suprimer").value;
+    let test = todolist.indexOf(valeur);
+    console.log(test);
+    todolist.splice(test, 1);
+    console.log(todolist);
+    
+    document.getElementById("input1").innerHTML = "";
+    
+    clean();
 
-    todolist.push(valeur);
-
-    const newTache = document.createElement("li");
-    let tache = document.getElementById("input1");
-    let k = tache.appendChild(newTache);
-    let lenght = todolist.length;
-    console.log(lenght);
-    let b=0;
-        for (let i = 0; i < lenght; i++)
-        {
-        k.innerHTML = todolist[b];
-        b++;
-        }
 }
